d67bbf159829515b8c7a251b7f5641ce3b12032e
tuhindasv0 <tuhindasv0@gmail.com>
2021-09-12 20:08:42 +0530

    token

--- authorizerLambda/Auth0/index.ts
+++ authorizerLambda/Auth0/index.ts
@@ -6,6 +6,9 @@ module.exports.handler = async (event, context, callback) => {
   console.log(" main handler event>>",event);
   console.log(" main handler  context>>",context);
 
+  var token=event.headers.Authorization;
+  console.log("Token >>",token);
+
   try {
     data = await lib.authenticate(event);
   }
